5f2c8be6594f843233728e6b3fe6c04ae0bf75c5
rdm315692400-lang <rdm315692400@gmail.com>
2026-09-23 09:31:40 +0300

    Delete app.config.py

--- app.config.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-MASSIVE_API_KEY=os.getenv("MASSIVE_API_KEY","")
-MASSIVE_BASE="https://api.massive.com"
-TICKERS=["MSTR","META","INTC","AMD","CSCO","COIN","AAPL","NVDA","PYPL","TSLA","MCD","NFLX","PLTR","AMZN","ADBE"]
