586f9dccaa7724976ed07d5d5f9de8d6e17f009c
rdm315692400-lang <rdm315692400@gmail.com>
2026-09-23 10:18:13 +0300

    Update firebase-config.example.js

--- app/static/firebase-config.example.js
+++ app/static/firebase-config.example.js
@@ -1,1 +1,10 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
 
+window.FIREBASE_VAPID_PUBLIC_KEY = "";
